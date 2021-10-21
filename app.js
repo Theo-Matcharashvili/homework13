be3a60a622759de73c407bb6ba2c66ac92935baf
Theo-Matcharashvili <90640276+Theo-Matcharashvili@users.noreply.github.com>
2021-10-21 16:46:42 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// Primitive types / values
+// String, Number, Boolean, Null, Undefined
+
+let str = 'My name is ';
+let str1 = `My name is `;
+let myName = 'Giorgi';
+
+// string concatenation
+// console.log(str + myName);
+// ES6
+// console.log(`${str} ${myName} My string `);
+
+let add = 100 + 50;
+let sub = 100 - 50;
+let multiply = 100 * 50;
+let div = 100 / 50;
+let mod = 10 % 2;
+
+console.log('mod - ',mod);
+console.log('add - ',add);
+console.log( 'sub - ', sub);
+console.log('multiply - ', multiply);
+console.log('div - ', div);
+
+let isAdult = true;
+let isTeenager = false;
+
+//asd asd asd asd asd
+isAdult = undefined;
+if(isAdult){
+    console.log('Yes I am Adult');
+} else {
+    console.log('Yes I am Teenager');
+}
+
+let nothing = null;
+let notDefined = undefined;
+
+// console.log(typeof str);
+// console.log(typeof myAge);
+// console.log(typeof isAdult);
+// console.log(typeof isTeenager);
+// console.log(nothing);
+// console.log(typeof notDefined);
